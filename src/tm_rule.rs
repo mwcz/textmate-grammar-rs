ec29ff3859e3ebfa6eefe4406d7ac656c34205a6
mwcz <mwc@clayto.com>
2023-08-14 23:02:45 -0400

    allow 'captures' values to have keys other than what is defined in the spec

--- src/tm_rule.rs
+++ src/tm_rule.rs
@@ -1,7 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt, marker::PhantomData, ops::Deref};
 
 use crate::{tm_patterns::TMPatterns, tm_regex::TMRegex};
-use serde::Deserialize;
+use serde::{
+    de::{MapAccess, Visitor},
+    Deserialize, Deserializer,
+};
 
 // TODO: implement mutual exclusivity of match & begin/end
 
@@ -18,11 +21,11 @@ pub struct TMRule<'a> {
     #[serde(rename = "contentName")]
     content_name: Option<&'a str>,
 
-    captures: Option<HashMap<u16, TMCapture<'a>>>,
+    captures: Option<TMCaptures<'a>>,
     #[serde(rename = "beginCaptures")]
-    begin_captures: Option<HashMap<u16, TMCapture<'a>>>,
+    begin_captures: Option<TMCaptures<'a>>,
     #[serde(rename = "endCaptures")]
-    end_captures: Option<HashMap<u16, TMCapture<'a>>>,
+    end_captures: Option<TMCaptures<'a>>,
 
     include: Option<&'a str>,
 
@@ -33,4 +36,63 @@ pub struct TMRule<'a> {
 #[allow(unused)]
 pub struct TMCapture<'a> {
     name: &'a str,
+}
+
+#[derive(Debug)]
+#[allow(unused)]
+pub struct TMCaptures<'a>(TMCaptureInner<'a>);
+type TMCaptureInner<'a> = HashMap<u16, TMCapture<'a>>;
+
+impl<'a> Deref for TMCaptures<'a> {
+    type Target = TMCaptureInner<'a>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'de, 'a> Deserialize<'de> for TMCaptures<'a>
+where
+    'de: 'a,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct TMCapturesVisitor<'a> {
+            _marker: PhantomData<&'a ()>,
+        }
+
+        impl<'de, 'a> Visitor<'de> for TMCapturesVisitor<'a>
+        where
+            'de: 'a,
+        {
+            type Value = TMCaptures<'a>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a map of u16 keys to TMCapture values")
+            }
+
+            fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                let mut map = HashMap::new();
+                while let Some(key) = access.next_key::<String>()? {
+                    if let Ok(u16_key) = key.parse::<u16>() {
+                        if let Ok(value) = access.next_value::<TMCapture<'a>>() {
+                            map.insert(u16_key, value);
+                        }
+                    } else {
+                        // Skip the value corresponding to the unparseable key
+                        let _: serde_json::Value = access.next_value()?;
+                    }
+                }
+                Ok(TMCaptures(map))
+            }
+        }
+
+        deserializer.deserialize_map(TMCapturesVisitor {
+            _marker: PhantomData,
+        })
+    }
 }
